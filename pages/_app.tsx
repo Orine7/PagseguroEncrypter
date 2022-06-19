e09dfd6ec76f725ca477578a91e0d2f5a4d5625b
Orine7 <eduardofilho_08@hotmail.com>
2022-06-19 15:03:01 -0300

    REmoved recaptcha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app'
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import { Slide, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-    >
+    <>
       <Component {...pageProps} />
       <ToastContainer
         position="top-center"
@@ -20,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         closeOnClick
         pauseOnHover
       />
-    </GoogleReCaptchaProvider>
+    </>
   )
 }
 
